4d49c5089982ab42cfc0e3ec18623dae7f1f6cee
Sushil Khera <Sushilkhera@gmail.com>
2022-02-04 00:56:09 +0530

    Initial commit with basic demo of Spring Eureka Server

---
+++ src/test/java/com/sushil/ms/SpringEurekaServer/SpringEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sushil.ms.SpringEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
